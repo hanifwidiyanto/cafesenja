f96d43ff15843214cec29a51c23815f7ffc81b9c
hanifwidiyanto <hanifwidiyanto.12@gmail.com>
2023-05-28 12:26:36 +0700

    set priority to img landing

--- src/app/landing.jsx
+++ src/app/landing.jsx
@@ -15,11 +15,11 @@ export default function Landing() {
             <div className='rounded-full p-4 w-fit h-fit bg-slate-500 bg-opacity-10 shadow-lg backdrop-filter backdrop-blur-lg absolute top-4 left-4 z-50'>
                 <Image src="/assets/images/logo.png" width={64} height={64} alt="" />
             </div>
-            <Image src="/assets/images/landing.png" layout="fill" objectFit='cover' alt="" className='saturate-50' />
+            <Image src="/assets/images/landing.png" layout="fill" objectFit='cover' alt="" className='saturate-50' priority />
             <div className='absolute bottom-6 w-full flex justify-center'>
                 <div className='w-10/12 bg-slate-500 bg-opacity-10 rounded-lg shadow-lg px-10 py-6 backdrop-filter backdrop-blur-lg flex flex-col gap-6 items-center text-center'>
                     <QuoteDisplay />
-                    
+
                     <div className='bg-amber-900 px-8 py-3 rounded-md flex gap-2 h-fit items-center'>
                         <span className='font-2xl font-medium'>Pesan Kopi</span>
                         <BsArrowRightShort className="relative top-1" />
